1809115e4d42004d6618847f508f2745a594d9d2
unknown <yasvanthhanumantu1@gmail.com>
2025-07-07 11:16:27 +0530

    secret files

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI, Request
+import os
+import shutil
+from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from agent import run_agent
+
+source_path = "/etc/secrets/service_account.json"
+destination_folder = "credentials"
+destination_path = os.path.join(destination_folder, "service_account.json")
+
+os.makedirs(destination_folder, exist_ok=True)
+
+if os.path.exists(source_path):
+    shutil.copy(source_path, destination_path)
+    print(f"✅ Copied service_account.json to {destination_path}")
 
 app = FastAPI()
 
